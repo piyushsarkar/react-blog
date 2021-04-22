8b711a9d47a7cbcccaae8b18906687d3f12f13a5
Piyush Sarkar <iampiyushsarkar@gmail.com>
2021-04-22 18:57:55 +0530

    fixed pages

--- src/components/Create.js
+++ src/components/Create.js
@@ -32,68 +32,73 @@ const Create = () => {
   };
 
   return (
-    <div className="create">
-      <h2>Add a new blog</h2>
-      <form onSubmit={handleSubmit}>
-        <label htmlFor="title">Blog title:</label>
-        <input
-          type="text"
-          name="title"
-          required
-          value={title}
-          onChange={(e) => setTitle(e.target.value)}
-        />
-        <label htmlFor="body">Blog Body:</label>
-        <textarea
-          name="body"
-          required
-          value={body}
-          onChange={(e) => setBody(e.target.value)}
-        ></textarea>
-        <label htmlFor="image">Blog image url:</label>
-        <input
-          type="url"
-          name="image"
-          required
-          value={image}
-          onChange={(e) => setImage(e.target.value)}
-        />
-        <label htmlFor="category">Blog category:</label>
-        <select value={category} onChange={(e) => setCategory(e.target.value)}>
-          <option value="Personal">Personal</option>
-          <option value="Books">Books</option>
-          <option value="Business">Business</option>
-          <option value="Cars">Cars</option>
-          <option value="DIY">DIY</option>
-          <option value="Entertainment">Entertainment</option>
-          <option value="Events">Events</option>
-          <option value="Fashion">Fashion</option>
-          <option value="Finance">Finance</option>
-          <option value="Fitness">Fitness</option>
-          <option value="Food">Food</option>
-          <option value="Gaming">Gaming</option>
-          <option value="Lifestyle">Lifestyle</option>
-          <option value="Music">Music</option>
-          <option value="Movies">Movies</option>
-          <option value="Politics">Politics</option>
-          <option value="Pets">Pets</option>
-          <option value="Sports">Sports</option>
-          <option value="Technology">Technology</option>
-          <option value="Travel">Travel</option>
-          <option value="Writing">Writing</option>
-        </select>
-        <label htmlFor="author">Blog author:</label>
-        <input
-          type="text"
-          name="author"
-          required
-          value={author}
-          onChange={(e) => setAuthor(e.target.value)}
-        />
+    <div className="container">
+      <div className="create">
+        <h2>Add a new blog</h2>
+        <form onSubmit={handleSubmit}>
+          <label htmlFor="title">Blog title:</label>
+          <input
+            type="text"
+            name="title"
+            required
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+          />
+          <label htmlFor="body">Blog Body:</label>
+          <textarea
+            name="body"
+            required
+            value={body}
+            onChange={(e) => setBody(e.target.value)}
+          ></textarea>
+          <label htmlFor="image">Blog image url:</label>
+          <input
+            type="url"
+            name="image"
+            required
+            value={image}
+            onChange={(e) => setImage(e.target.value)}
+          />
+          <label htmlFor="category">Blog category:</label>
+          <select
+            value={category}
+            onChange={(e) => setCategory(e.target.value)}
+          >
+            <option value="Personal">Personal</option>
+            <option value="Books">Books</option>
+            <option value="Business">Business</option>
+            <option value="Cars">Cars</option>
+            <option value="DIY">DIY</option>
+            <option value="Entertainment">Entertainment</option>
+            <option value="Events">Events</option>
+            <option value="Fashion">Fashion</option>
+            <option value="Finance">Finance</option>
+            <option value="Fitness">Fitness</option>
+            <option value="Food">Food</option>
+            <option value="Gaming">Gaming</option>
+            <option value="Lifestyle">Lifestyle</option>
+            <option value="Music">Music</option>
+            <option value="Movies">Movies</option>
+            <option value="Politics">Politics</option>
+            <option value="Pets">Pets</option>
+            <option value="Sports">Sports</option>
+            <option value="Technology">Technology</option>
+            <option value="Travel">Travel</option>
+            <option value="Writing">Writing</option>
+          </select>
+          <label htmlFor="author">Blog author:</label>
+          <input
+            type="text"
+            name="author"
+            required
+            value={author}
+            onChange={(e) => setAuthor(e.target.value)}
+          />
 
-        {!isPending && <button>Add Blog</button>}
-        {isPending && <button disabled>Adding Blog</button>}
-      </form>
+          {!isPending && <button>Add Blog</button>}
+          {isPending && <button disabled>Adding Blog</button>}
+        </form>
+      </div>
     </div>
   );
 };
